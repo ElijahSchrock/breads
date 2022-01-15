761f8b47b6fa7ad8a66e5fbb613a12db3d40f164
Elijah Schrock <70502486+ElijahSchrock@users.noreply.github.com>
2022-01-15 10:09:24 -0800

    render a 404 view

---
+++ views/404.jsx
@@ -0,0 +1,15 @@
+const React = require('react');
+const Default = require('../views/layouts/Default');
+
+function error404 () {
+    return (
+        <Default>
+            <main>
+                <h1> 404: PAGE NOT FOUND -__-</h1>
+                <p>Got Dammit, Randy lost this page again!</p>
+            </main>
+        </Default>
+    )
+}
+
+module.exports = error404
