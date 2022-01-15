23ae64309ac4195b0952f3e0d6361b1e9a043e4b
Elijah Schrock <70502486+ElijahSchrock@users.noreply.github.com>
2022-01-15 14:00:37 -0800

    create and new routes complete

--- server.js
+++ server.js
@@ -8,12 +8,18 @@ const PORT = process.env.PORT;
 const app = express();
 
 //MIDDLEWARE || Before the clients request hit the path. It gets intercepted through middleware and does this process.
+
 app.set('views', __dirname + '/views') //defining which folder were putting the view files inside
+
 app.set('view engine', 'jsx') //setting the view engine ((jsx))
+
 app.engine('jsx', require('express-react-views').createEngine()); //intializing the jsx
+
 //adding CSS
 app.use(express.static('public'))
 
+//urlencoded
+app.use(express.urlencoded({extended: true}))
 //ROUTES
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!');
